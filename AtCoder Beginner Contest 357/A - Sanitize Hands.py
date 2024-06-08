7977462e8746ec441fb104d87877e3be4276ce52
Kai_T <167957877+Kai-T-KiA@users.noreply.github.com>
2024-06-08 22:13:02 +0900

    Create A - Sanitize Hands.py

---
+++ AtCoder Beginner Contest 357/A - Sanitize Hands.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+
+H = list(map(int, input().split()))
+
+count = 0
+
+for i in range(N):
+  if N == 1:
+    if M >= H[i]:
+      print(1)
+    else:
+      print(0)
+    exit()
+
+
+  
+  M -= H[i]
+
+  if M >= 0:
+    count += 1
+  else:
+    break
+
+print(count)
