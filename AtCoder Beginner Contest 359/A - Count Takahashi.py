b77334b8ee62282e4cea353702f53c762d705192
Kai_T <167957877+Kai-T-KiA@users.noreply.github.com>
2024-07-01 21:53:29 +0900

    Create A - Count Takahashi.py

---
+++ AtCoder Beginner Contest 359/A - Count Takahashi.py
@@ -0,0 +1,13 @@
+# 問題URL https://atcoder.jp/contests/abc359/tasks/abc359_a
+
+N = int(input())
+
+S = [input() for _ in range(N)]
+
+count = 0
+
+for s in S:
+  if s == 'Takahashi':
+    count += 1
+
+print(count)
